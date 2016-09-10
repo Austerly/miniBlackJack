6939b4976d16d8d2af05339747e376d4ff696911
Austerly <austin.hesterly@gmail.com>
2016-09-10 10:24:29 -0600

    updated with working API call

---
+++ miniBlackJack/NetworkController.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkController.swift
+//  miniBlackJack
+//
+//  Created by Austin Hesterly on 9/10/16.
+//  Copyright © 2016 Austin Hesterly. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController {
+    
+    static func dataAtURL(urlString: String, completion: (data: NSData?) -> Void) {
+        guard let url = NSURL(string: urlString) else { completion(data: nil); return}
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (dataFromURL, _, error) in
+            if let error = error {
+                print(error)
+                print(error.localizedDescription)
+                print("This error is in the NetworkController in the dataAtURL Method")
+                completion(data: nil)
+                return
+            }
+            
+            completion(data: dataFromURL)
+        
+            
+        }
+        task.resume()
+    }
+    
+    static func jsonFromData(data: NSData) -> [String : AnyObject]? {
+        let json = try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? [String : AnyObject]
+        return json ?? nil
+    }
+    
+    //Put this in the plist
+    /*
+    <key>NSAppTransportSecurity</key>
+    <dict>
+    <!--Include to allow all connections (DANGER)-->
+    <key>NSAllowsArbitraryLoads</key>
+    <true/>
+    </dict>
+     */
+}
--- miniBlackJack/ViewController.swift
+++ miniBlackJack/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+    
+    @IBAction func playButtonTapped(sender: AnyObject) {
+        NetworkController.dataAtURL("http://deckofcardsapi.com/api/deck/new/draw/?count=2") { (data) in
+            print("data:", data)
+            guard let data = data else {return}
+            print("NetworkController JSON from Data: ", NetworkController.jsonFromData(data))
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
